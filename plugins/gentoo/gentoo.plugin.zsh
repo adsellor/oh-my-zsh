c58b84749d2f9d7e0b69948eea9baa089934714f
adsellor <adsellor@protonmail.com>
2018-04-12 15:07:04 +0400

    Add initial gentoo plugin

---
+++ plugins/gentoo/gentoo.plugin.zsh
@@ -0,0 +1,8 @@
+# Emerge 
+alias ems="emerge --search"
+alias emsd="emerge --searchdesc"
+alias es="sudo emerge --sync" 
+alias ew="sudo emerge-webrsync"
+alias euw="sudo emerge --ask --update --newuse --deep --with-bdeps=y @world"
+alias euwq="sudo emerge --ask --update --newuse --deep --quiet --with-bdeps=y @world"
+alias emd="sudo emerge --ask --depclean"
